8585012385330b6b64d44019dee889f580ed146e
Gemma Park <156281707+gemmap520@users.noreply.github.com>
2024-10-17 16:12:28 -0400

    Update 14_LongestCommonPrefix.py

--- 14_LongestCommonPrefix.py
+++ 14_LongestCommonPrefix.py
@@ -1,1 +1,11 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = ""
 
+        for i in range(len(strs[0])):
+            prefix_temp = strs[0][i]
+            for j in strs:
+                if i >= len(j) or j[i] != prefix_temp:
+                    return prefix
+            prefix += prefix_temp
+        return prefix
