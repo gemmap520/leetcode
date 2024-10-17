86b48eeee631162a308af1e207d57248304df67a
Gemma Park <156281707+gemmap520@users.noreply.github.com>
2024-10-17 16:16:13 -0400

    Update 9_Palindrome_Number.py

--- 9_Palindrome_Number.py
+++ 9_Palindrome_Number.py
@@ -1,1 +1,15 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0:
+            return False
+
+        lst = []
+        while x > 0:
+            lst.append(x % 10)
+            x = x // 10
+        length = len(lst) - 1
+        for i in range(len(lst) - 1):
+            if lst[i] != lst[length - i]:
+                return False
+        return True
 
